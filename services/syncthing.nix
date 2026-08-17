29140633fd820b45f97900351bbcd5b165d118b7
Sophron Ragozin <sophronfj@gmail.com>
2026-08-17 23:51:07 +0500

    add reverse proxy for syncthing

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -1,12 +1,22 @@
 { ... }: {
-  services.syncthing = {
-    enable = true;
-    user = "server";
-    group = "users";
-    dataDir = "/home/server";
-    openDefaultPorts = true;
-    guiAddress = "0.0.0.0:8384";
-    overrideDevices = false;
-    overrideFolders = false;
+  services = {
+    syncthing = {
+      enable = true;
+      user = "server";
+      group = "users";
+      dataDir = "/home/server";
+      openDefaultPorts = true;
+      guiAddress = "127.0.0.1:8384";
+      overrideDevices = false;
+      overrideFolders = false;
+    };
+    nginx.virtualHosts."syncthing.server.local" = {
+      forceSSL = false;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8384";
+        proxyWebsockets = true;
+        recommendedProxySettings = true;
+      };
+    }
   };
 }
